540db86aa9c7d09e4c9fa0ee10a4154afd480bab
kyaneyang <yangzidong@arizona.edu>
2024-11-22 15:48:28 -0700

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,37 +1,3 @@
-import pickle
-from sklearn.datasets import fetch_california_housing
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
-
-
-# Load dataset
-data = fetch_california_housing()
-X, y = data.data, data.target
-
-# Split the data
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Train the model
-model = RandomForestRegressor(random_state=42)
-model.fit(X_train, y_train)
-
-
-from sklearn.metrics import mean_squared_error
-from sklearn.metrics import r2_score
-
-# Evaluate the model
-y_pred = model.predict(X_test)
-mse = mean_squared_error(y_test, y_pred)
-print(f"Mean Squared Error: {mse}")
-r2_score = r2_score(y_test, y_pred)
-print(f"R2 Score: {r2_score}")
-
-# Save the model
-import pickle
-with open("regression_model.pkl", "wb") as file:
-    pickle.dump(model, file)
-
-
 import streamlit as st
 import pickle
 import numpy as np
